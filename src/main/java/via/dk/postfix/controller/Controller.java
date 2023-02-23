090d83f7f8ddb465c04f673a2abca94ca436b606
Semo0 <317546@via.dk>
2023-02-23 13:43:05 +0100

    base

---
+++ src/main/java/via/dk/postfix/controller/Controller.java
@@ -0,0 +1,11 @@
+package via.dk.postfix.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/PostFix")
+public class Controller {
+
+
+
+}
